55708b2e6a5cb2b1d0a42a517f6027239e9c7716
yenle73 <128300242+yenle73@users.noreply.github.com>
2023-12-08 10:48:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ re = st.radio('Choose research', [0, 1])
 
 if re:
   if st.button('Predict'):
-    feature_vector = np.array([gre, toefl, uni_rate, sop, lor, cgpa, research]).reshape(1,-1)
+    feature_vector = np.array([gre, toefl, uni_rate, sop, lor, cgpa, re]).reshape(1,-1)
     result = str((model.predict(feature_vector)[0])[0])
     st.header('Result')
     st.write(result)
